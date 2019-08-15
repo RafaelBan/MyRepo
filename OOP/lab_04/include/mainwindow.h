43cfca36cb799cb1fdf98c8947d36fed44635ad6
rflban <nabiev.alsn@yandex.ru>
2019-08-15 23:54:53 +0300

    initialiaze project

---
+++ OOP/lab_04/include/mainwindow.h
@@ -0,0 +1,23 @@
+#ifndef MAINWINDOW_H_
+#define MAINWINDOW_H_
+
+#include <QMainWindow>
+
+namespace Ui
+{
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT;
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    virtual ~MainWindow();
+
+private:
+};
+
+#endif // MAINWINDOW_H_
+
